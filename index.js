e3e33c0690af03be5ae6fe01d2c6cd48638375e4
turzacse <turzacse@gmail.com>
2023-11-08 21:05:21 +0600

    update origin for load data

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors({
-  origin: "*",
+  origin: [
+    'https://tastebud-tavern.web.app',
+    'https://tastebud-tavern.firebaseapp.com',
+    'http://localhost:5173'
+  ],
   credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
 
-// [
-//   'https://tastebud-tavern.web.app',
-//   'https://tastebud-tavern.firebaseapp.com',
-//   '*'
-// ]
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bnzewy6.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
